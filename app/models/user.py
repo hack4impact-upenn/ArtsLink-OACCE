3d480c48236be608e51fac6c90c3b5f33a2f50a4
brandon-rep <brandon@represently.com>
2018-02-21 21:53:16 -0500

    create many2many table and delete fields

--- app/models/user.py
+++ app/models/user.py
@@ -11,33 +11,34 @@ class Permission:
     GENERAL = 0x01
     ADMINISTER = 0xff
 
-tags = db.Table('tags',
-    db.Column('tag_id', db.Integer, db.ForeignKey('tag.id'), primary_key=True),
-    db.Column('organizations_id', db.Integer, db.ForeignKey('organizations.id'), primary_key=True)
-)
 class Organization(db.Model):
     __tablename__ = 'organizations'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
     email = db.Column(db.String(64), nullable=False)
     phone = db.Column(db.Integer, nullable=False)
-    street_number = db.Column(db.Integer)
-    street = db.Column(db.String(64))
-    zipcode = db.Column(db.Integer)
-    city = db.Column(db.String(64))
-    state = db.Column(db.String(64))
+    address = db.Column(db.String(500))
     website_link = db.Column(db.String(120))
     hours = db.Column(db.String(64))
-    services_offered =  db.Column(db.String(500))
     description = db.Column(db.Text)
-    tags = db.relationship('Tag', secondary=tags, lazy='subquery',
-        backref=db.backref('organizations', lazy=True))
-    default = db.Column(db.Boolean, default=False, index=True)
-    permissions = db.Column(db.Integer)
+    tags = db.relationship("TagAssociation", back_populates="organizations")
 
 class Tag(db.Model):
+    __tablename__ = 'tag'
     id = db.Column(db.Integer, primary_key=True)
     tag_name = db.Column(db.String(64), nullable=False)
+    services_offered =  db.Column(db.String(500))
+    accessibility = db.Column(db.String(64))
+    ages = db.Column(db.String(64))
+    organizations = db.relationship("TagAssociation", back_populates="tag")
+
+class TagAssociation(db.Model):
+    __tablename__ = 'tagassociation'
+    id = db.Column(db.Integer, primary_key=True)
+    organiztion_id = db.Column(db.Integer, db.ForeignKey('organizations.id', ondelete='CASCADE'))
+    tag_id = db.Column(db.Integer, db.ForeignKey('tag.id', ondelete='CASCADE'))
+    tag = db.relationship("Tag", back_populates="organizations")
+    vendor = db.relationship("Organization", back_populates="tags")
 
 class Role(db.Model):
     id = db.Column(db.Int
